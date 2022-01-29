8e6534b8a1e1b215d9a1258dadb58692dcaf5752
ea42ru <ea42ru@mail.ru>
2022-01-29 23:40:56 +0300

    v 0.04 Скелет (заготовка).

---
+++ install_SimplyService.bat
@@ -0,0 +1,12 @@
+set PR_PATH=%CD%
+set JavaHome="P:\IdeaProjects\jre8_111\"
+SET PR_SERVICE_NAME=SimplyService
+SET PR_JAR=SimplyService.jar
+SET START_CLASS=ru.ea42.EaServices.TestSymplyStart
+SET START_METHOD=start
+SET STOP_CLASS=ru.ea42.EaServices.TestSymplyStart
+SET STOP_METHOD=stop
+SET JVM_OPTIONS=-Dapp.home=%PR_PATH%
+
+prunsrv.exe //DS//%PR_SERVICE_NAME%
+prunsrv.exe //IS//%PR_SERVICE_NAME% --Install="%PR_PATH%\prunsrv.exe" --Jvm=auto --Startup=manual --StartMode=jvm --StartClass=%START_CLASS% --StartMethod=%START_METHOD% --StopMode=jvm --StopClass=%STOP_CLASS% --StopMethod=%STOP_METHOD% --Classpath="%PR_PATH%\%PR_JAR%" --DisplayName="%PR_SERVICE_NAME%" ++JvmOptions=%JVM_OPTIONS% --JavaHome=%JavaHome%
